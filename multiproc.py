86e57d38394aa4ba6886175ff66b7dcd385c362d
karlosss <los.karlosss@gmail.com>
2014-12-16 11:16:03 +0100

    Update multiproc.py

--- multiproc.py
+++ multiproc.py
@@ -1,41 +1,34 @@
-import time
-from multiprocessing import Process
-from multiprocessing.sharedctypes import Value, Array
-from ctypes import c_bool, c_int, c_wchar_p
+import threading
+from time import sleep
 
-def func():
-    for i in range(100):
-        time.sleep(0.01)
-        v[0] += 1
+promenna = 0
 
-def func2():
-    for i in range(10000,100000,1000):
-        time.sleep(0.03)
-        print(i)
-    print("ahoj!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
-    v[1] = 200
-    for i in v:
-        print i
-    a = "lokalni promenna"
-    print(a)
-    print b.value
-    print s.value
+def a():
+    global promenna
+    for i in range(30):
+        sleep(0.3)
+        promenna = promenna + 1
+        print(promenna)
+
+def b():
+    global promenna
+    for j in range(10000,10030,1):
+        sleep(0.2)
+        promenna = promenna + 1
+        print(promenna)
 
 
 
-v = Array(c_int, [1,2,3,4,5])
-b = Value(c_bool, True)
-s = Value(c_wchar_p, "asdf")
+t1 = threading.Thread(target=a)
+t2 = threading.Thread(target=b)
 
-p = Process(target=func)
-p.start()
-p2 = Process(target=func2)
-p2.start()
+t1.start()
+t2.start()
 
+t1.join()
+t2.join()
 
-
-
-
-
-
+print("")
+print()
+print(promenna)
 
